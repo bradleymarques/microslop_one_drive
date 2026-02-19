567e4fed1884796a74848bc77ce3841f63cb8cf8
Bradley Marques <bradleyrcmarques@gmail.com>
2026-02-19 16:30:24 +0200

    Adds created_at and updated_at to drives

--- lib/microslop_one_drive/drive.rb
+++ lib/microslop_one_drive/drive.rb
@@ -1,14 +1,17 @@
 module MicroslopOneDrive
   class Drive
-    attr_reader :id, :name, :url, :drive_type
+    attr_reader :identifier, :name, :url, :drive_type, :created_at, :updated_at
 
     def initialize(drive_hash)
       @drive_hash = drive_hash
 
-      @id = drive_hash.fetch("id", nil)
+      @identifier = drive_hash.fetch("id", nil)
       @name = drive_hash.fetch("name", nil)
       @url = drive_hash.fetch("webUrl", nil)
       @drive_type = drive_hash.fetch("driveType", nil)
+
+      @created_at = Utils.safe_parse_time(drive_hash.fetch("createdDateTime", nil))
+      @updated_at = Utils.safe_parse_time(drive_hash.fetch("lastModifiedDateTime", nil))
     end
   end
 end
--- test/drive_test.rb
+++ test/drive_test.rb
@@ -16,10 +16,12 @@ module MicroslopOneDrive
 
       drive = @client.drive(drive_id: @drive_id)
       assert_kind_of MicroslopOneDrive::Drive, drive
-      assert_equal "0f097864e0cfea42", drive.id
+      assert_equal "0f097864e0cfea42", drive.identifier
       assert_equal "OneDrive", drive.name
       assert_equal "https://my.microsoftpersonalcontent.com/personal/0f097864e0cfea42/Documents", drive.url
       assert_equal "personal", drive.drive_type
+      assert_equal Time.parse("2026-02-16T05:06:27Z"), drive.created_at
+      assert_equal Time.parse("2026-02-19T07:39:49Z"), drive.updated_at
     end
   end
 end
