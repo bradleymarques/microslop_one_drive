98f068dcae6fa6f98225deb34964731fa26ce0bc
Bradley Marques <bradleyrcmarques@gmail.com>
2026-02-23 09:08:31 +0200

    Adds tests

--- lib/microslop_one_drive/client.rb
+++ lib/microslop_one_drive/client.rb
@@ -1,10 +1,16 @@
+require "json"
+
 module MicroslopOneDrive
   class Client
     BASE_URL = "https://graph.microsoft.com/v1.0".freeze
     BATCH_REQUEST_LIMIT = 20.freeze
 
-    def initialize(access_token)
+    # @param access_token [String] OAuth access token for Microsoft Graph.
+    # @param logger [Object, nil] Optional logger (e.g. Rails.logger) that responds to +#info+, +#debug+, +#warn+, +#error+.
+    #   When set, all API requests and responses are logged.
+    def initialize(access_token, logger: nil)
       @access_token = access_token
+      @logger = logger
 
       @headers = {
         "Authorization" => "Bearer #{@access_token}",
@@ -164,11 +170,48 @@ module MicroslopOneDrive
     private
 
     def get(path:, query: {})
-      HTTParty.get("#{BASE_URL}/#{path}", headers: @headers, query: query)
+      url = "#{BASE_URL}/#{path}"
+
+      log_request("GET", url, query: query)
+
+      response = HTTParty.get(url, headers: @headers, query: query)
+
+      log_response(response, "GET", url)
+
+      response
     end
 
     def post(path:, body:)
-      HTTParty.post("#{BASE_URL}/#{path}", headers: @headers, body: body)
+      url = "#{BASE_URL}/#{path}"
+
+      log_request("POST", url, body: body)
+
+      response = HTTParty.post(url, headers: @headers, body: body)
+
+      log_response(response, "POST", url)
+
+      response
+    end
+
+    def log_request(method, url, query: nil, body: nil)
+      return unless @logger
+
+      @logger.info ""
+      @logger.info "==================== START MicroslopOneDrive #{method} #{url} ===================="
+      @logger.info "Request method: #{method}"
+      @logger.info "Request url: #{url}"
+      @logger.info "Request query: #{query.inspect}" if query && query.any?
+      @logger.info "Request body: #{body.inspect}" if body
+    end
+
+    def log_response(response, method, url)
+      return unless @logger
+
+      @logger.info ""
+      @logger.info "Response code: #{response.code}"
+      @logger.info "Response body:" if response.parsed_response
+      @logger.info JSON.pretty_generate(response.parsed_response) if response.parsed_response
+      @logger.info "==================== END MicroslopOneDrive #{method} #{url} ===================="
     end
 
     def handle_error(response)
