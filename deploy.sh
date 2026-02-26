054cad26764262aebbedbabdb844eb2374bea458
Bradley Marques <bradleyrcmarques@gmail.com>
2026-02-26 12:45:14 +0200

    Deploy script

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+version=$(ruby -e "load 'lib/microslop_one_drive/version.rb'; puts MicroslopOneDrive::VERSION")
+
+git tag -a v$version -m "Release $version"
+git push origin v$version
+
+gem build microslop_one_drive.gemspec
+gem push microslop_one_drive-$version.gem
